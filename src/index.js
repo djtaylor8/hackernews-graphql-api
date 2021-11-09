3d78e176f476c4cbd84f31569249279d08c11486
DJ Taylor <dtaylor88@gmail.com>
2021-11-09 10:47:17 -0800

    connect prisma client to db and update resolvers

--- src/index.js
+++ src/index.js
@@ -1,67 +1,65 @@
 const { ApolloServer } = require('apollo-server');
+const { PrismaClient } = require('@prisma/client');
 const fs = require('fs');
 const path = require('path');
+const prisma = new PrismaClient();
 
-let links = [{
-    id: 'link-0',
-    url: 'www.howtographql.com',
-    description: 'Fullstack tutorial for GraphQL'
-}]
 
-//2
 const resolvers = {
     Query: {
         info: () => `This is the API of a Hackernews clone`,
-        feed: () => links,
-        link: (_, {id}) => {
-            const link = links.find(link => link.id === id)
-            return link;
-        }
+        feed: async (parent, args, context) => {
+            return context.prisma.link.findMany()
+        },
+        // link: (_, {id}) => {
+        //     const link = links.find(link => link.id === id)
+        //     return link;
+        // }
     },
     Mutation: {
-        post: (parent,args) => {
-            let idCount = links.length 
-
-                const link = {
-                    id: `link-${idCount++}`,
-                    description: args.description,
+        post: (parent,args,context,info) => {
+            const newLink = context.prisma.link.create({
+                data: {
                     url: args.url,
-                }
-                links.push(link)
-                return link
+                    description: args.descriptions,
+                },
+            })
+            return newLink;
         },
-        updateLink: (parent,args) => {
-            const link = links.find(link => link.id === args.id);
-            if (!link) {
-                throw new Error(`Couldn't find link with id ${args.id}`);
-            }
-            if (args.description !== undefined) {
-                link.description = args.description;
-            }
-            if (args.url !== undefined) {
-                link.url = args.url;
-            }
-            return link;
-        },
-        deleteLink: (parent,args) => {
-            const link = links.find(link => link.id === args.id);
-            if (!link) {
-                throw new Error(`Couldn't find link with id ${args.id}`);
-            }
-            const linkIdx = links.findIndex(link => link.id === args.id);
-            links.splice(linkIdx, 1);
-            return link;
-        }
-    },
+        // updateLink: (parent,args) => {
+        //     const link = links.find(link => link.id === args.id);
+        //     if (!link) {
+        //         throw new Error(`Couldn't find link with id ${args.id}`);
+        //     }
+        //     if (args.description !== undefined) {
+        //         link.description = args.description;
+        //     }
+        //     if (args.url !== undefined) {
+        //         link.url = args.url;
+        //     }
+        //     return link;
+        // },
+    //     deleteLink: (parent,args) => {
+    //         const link = links.find(link => link.id === args.id);
+    //         if (!link) {
+    //             throw new Error(`Couldn't find link with id ${args.id}`);
+    //         }
+    //         const linkIdx = links.findIndex(link => link.id === args.id);
+    //         links.splice(linkIdx, 1);
+    //         return link;
+    //     }
+     },
 }
 
-//3
 const server = new ApolloServer({
     typeDefs: fs.readFileSync(
         path.join(__dirname, 'schema.graphql'),
         'utf8'
     ),
     resolvers,
+    context: {
+        prisma,
+    }
 })
 
 server
