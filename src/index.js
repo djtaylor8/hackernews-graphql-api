a09d65ca785c1b0da9ee0960c6f69108b663c68b
DJ Taylor <dtaylor88@gmail.com>
2021-11-09 08:44:07 -0800

    update resolver function to include links, add dummy data

--- src/index.js
+++ src/index.js
@@ -12,12 +12,24 @@ const typeDefs = `
         url: String!
     }
 `
+let links = [{
+    id: 'link-0',
+    url: 'www.howtographql.com',
+    description: 'Fullstack tutorial for GraphQL'
+}]
 
 //2
 const resolvers = {
     Query: {
-        info: () => `This is the API of a Hackernews clone`
+        info: () => `This is the API of a Hackernews clone`,
+        feed: () => links,
+    },
+    Link: {
+        id: (parent) => parent.id,
+        description: (parent) => parent.description,
+        url: (parent) => parent.url,
     }
+
 }
 
 //3
