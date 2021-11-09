eb791611d21c75f60b5c6532ed50b1440fef86e4
DJ Taylor <dtaylor88@gmail.com>
2021-11-08 23:05:42 -0800

    basic graphql setup

--- src/index.js
+++ src/index.js
@@ -1,1 +1,27 @@
 const { ApolloServer } = require('apollo-server');
+
+//1
+const typeDefs = `
+    type Query {
+        info: String!
+    }
+`
+
+//2
+const resolvers = {
+    Query: {
+        info: () => `This is the API of a Hackernews clone`
+    }
+}
+
+//3
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server
+    .listen()
+    .then(({ url }) =>
+    console.log(`Server is running on ${url}`)
+    );
