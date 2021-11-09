f620c855ff6b4fb9bc379b699db5a8925306431c
DJ Taylor <dtaylor88@gmail.com>
2021-11-09 11:14:34 -0800

    enable req headers in context header

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const { ApolloServer } = require('apollo-server');
 const { PrismaClient } = require('@prisma/client');
+const { getUserId } = require('./utils');
 const fs = require('fs');
 const path = require('path');
 const prisma = new PrismaClient();
@@ -57,10 +58,15 @@ const server = new ApolloServer({
         'utf8'
     ),
     resolvers,
-    context: {
-        prisma,
+    context: ({ req }) => {
+        return {
+            ...req,
+            prisma,
+            userId:
+                req && req.headers.authorization ? getUserId(req) : null
+        };
     }
-})
+});
 
 server
     .listen()
